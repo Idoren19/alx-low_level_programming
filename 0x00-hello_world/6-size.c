5fdb93a451d83a18436017fe85563b209b92537a
Anietie Idorenyin <anietieidoren@gmail.com>
2023-02-16 19:12:49 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,18 +3,11 @@
  * main - various types of computer
  * Return: 0
  */
-int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float f;
-
-	printf("size of char: %i byte(s)\n", sizeof(a));
-printf("size of int %i byte(s)\n", sizeof(b));
-printf("size of long int: %i byte(s)\n", sizeof(c));
-printf("size of long long int: %i byte(s)\n", sizeof(d));
-printf("size of float: %i byte(s)\n", sizeof(f));
+	printf("size of char: %lu byte(s)\n", sizeof(char));
+	printf("size of int: %lu byte(s)\n", sizeof(int));
+	printf("size of long int: %lu byte(s)\n", sizeof(long int));
+	printf("size of long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("size of float: %lu byte(s)\n", sizeof(float));
 return (0);
 }
