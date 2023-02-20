93ff215ff57f33de2d533c5c52da36a25840a2c3
Anietie Idorenyin <anietieidoren@gmail.com>
2023-02-20 09:08:28 -0800

    print alph

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,7 +8,8 @@
 int main(void)
 {
 	char h;
-	for (h = 'a'; h <= 'z';  h++)
+
+	for (h = 'a' ; h <= 'z' ;  h++)
 		putchar(h);
 	putchar("\n");
 		return (0);
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main fuucion
+ * Return: 0
+ */
+
+int main(void)
+{
+	char h;
+
+	for (h = 'a' ; h <= 'z' ; h++)
+		putchar(h);
+	for (h = 'A' ; h <= 'Z' ; h++)
+		putchar(h);
+	putchar('\n');
+		return (0);
+}
