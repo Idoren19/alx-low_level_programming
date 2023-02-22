02f50e7fd8abf55a700eae87cf7b095c10e23321
Anietie Idorenyin <anietieidoren@gmail.com>
2023-02-22 07:09:22 -0800

    print_alp

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - main function
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char h;
+
+	for (h = 'a'; h <= 'z'; h++)
+		_putchar(h);
+	_putchar('\n');
+
+}
